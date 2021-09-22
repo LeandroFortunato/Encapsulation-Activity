0b33749668db55ee91b032109057f28a5b879f85
Leandro Fortunato <robertomax@yahoo.com>
2021-09-22 03:55:12 +0100

    Class Diagram & Encapsulation Activity

---
+++ Student.java
@@ -0,0 +1,49 @@
+public class Student {
+    private String stdID;
+    private String stdName;
+    private String stdGrade;
+    private int stdGroup;
+    private String stdSecretNickName;
+
+      
+    public Student(String pID,String pName,String pGrade, int pGroup, String pSecretNickName){
+        this.stdID = pID;
+        this.stdName = pName;
+        this.stdGrade = pGrade;
+        this.stdGroup = pGroup;
+        this.stdSecretNickName = pSecretNickName;
+        }
+ 
+    public String getStudentGrade(){
+          return stdGrade;    
+    }
+
+    private boolean setStudentGrade(String pGrade){
+        if ("ABCDEF".indexOf(pGrade) == -1) {
+           return false;
+        }
+        stdGrade = pGrade;
+        return true;
+    }
+
+    public String getStudentName(){
+        return stdName;    
+
+    }
+    private boolean setStudentGroup(int pGroup){
+            if (pGroup < 1 || pGroup > 5) {
+               return false;
+            }
+            stdGroup = pGroup;
+            return true;
+        }
+  }
+
+    private String getMySecretNickName(){
+        return stdSecretNickName;    
+    }
+    private void setMySecretNickName(String pSecretNickName){
+        stdSecretNickName = pSecretNickName;
+        return ;       
+    }
+}
